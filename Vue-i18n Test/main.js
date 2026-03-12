d37719902bc4d851135121e9ae73a7587bf26f42
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-03-12 13:27:32 +0100

    main.js i18n Test

---
+++ Vue-i18n Test/main.js
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createI18n } from 'vue-i18n'
+
+
+import de from './sprachen/de.json'
+import en from './sprachen/en.json'
+
+
+
+const i18n = createI18n({
+    locale: 'de', // Standard-Sprache
+    messages: {
+        de,
+        en
+    }
+})
+
+
+createApp(App).use(i18n).mount('#app')
+
