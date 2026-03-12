afcb809d8188ae3ba9aea4ae68ed97c8e8be168a
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-03-12 13:26:46 +0100

    App.vue i18n Test

---
+++ Vue-i18n Test/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ $t('welcome') }}</h1>
+    <p>{{ $t('login') }}</p>
+    <button @click="changesprache('en')">{{ $t('sprache') }} (EN)</button>
+    <button @click="changesprache('de')">{{ $t('sprache') }} (DE)</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    changesprache(sprache) {
+      this.$i18n.locale = sprache
+      localStorage.setItem('sprache', sprache)
+    }
+  },
+  created() {
+    const savedsprache = localStorage.getItem('sprache')
+    if (savedsprache) {
+      this.$i18n.locale = savedsprache
+    }
+  }
+}
+</script>
