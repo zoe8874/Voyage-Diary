1e3905381138c5f12b15b11d5b2244acc1e6e107
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-04-02 12:00:47 +0200

    Version 002
    feat(frontend): i18n erweitert und Registrierungsseite wird vollständig übersetzt
    
    - Sprach-JSON-Struktur erweitert
    - Registrierung.vue wird vollständig übersetzt und ist HTML technisch fertig
    - Sprachumschalter in eigene wiederverwendbare Komponente ausgelagert

---
+++ Version 002/views/browseView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <body>
+
+  <div class="searchBar">
+    <input type="text" placeholder="Search..">
+
+
+  </div>
+  </body>
+
+</template>
+
+<style scoped>
+
+</style>
