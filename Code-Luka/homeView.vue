0381124c5082fa05653a5ee0546c3412f73c66a7
Luka Vucer <vucerluka@gmail.com>
2026-04-30 12:04:37 +0200

    feat: Homepage weiterbearbeitet
    mehrere Buttons + misc.
    Notiz: Verbindung muss bald passieren.

--- Code-Luka/homeView.vue
+++ Code-Luka/homeView.vue
@@ -27,13 +27,38 @@
 
       </div>
       <div id="button-upper-banner">
+        <button id="join"> Join over 10'000 Nomads worldwide!</button>
+        <p id="upper-text"> Your Journey, Their Inspiration </p>
+        <p id="lower-text"> voyage-diary is where nomads share authentic travel experiences, insider tips, and hidden gems from around the world.</p>
         <button id="start"> Start your Diary → </button>
         <button id="explore"> 🌐 Explore </button>
+
       </div>
     </div>
   </div>
 
   <div id="upper-lower-banner">
+
+    <p id="why"> Why Nomads Love Us</p>
+    <p id="more"> More than just a travel blog. It's a community of adventurers sharing real experiences.</p>
+
+    <div id="left-square">
+
+    
+
+    </div>
+
+    <div id="center-square">
+
+
+
+    </div>
+
+    <div id="right-square">
+
+
+
+    </div>
 
   </div>
 
@@ -56,6 +81,41 @@
 </template>
 
 <style scoped>
+
+#join{
+  background-color: white;
+  padding: 10px;
+  border: none;
+  color: black;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  border-radius: 15px;
+}
+
+#upper-text{
+  color: #b5acfe;
+  font-size: 12px;
+  text-align: center;
+  text-decoration: none;
+  text-decoration: none;
+  padding: 5px;
+  margin: 5px;
+  font-family: arial,serif;
+}
+
+#lower-text{
+  color: black;
+  font-size: 12px;
+  text-align: center;
+  text-decoration: none;
+  text-decoration: none;
+  padding: 5px;
+  margin: 5px;
+  font-family: arial,serif;
+}
 
 #start{
   background-color: #5959f7;
