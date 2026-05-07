5a9c5ce3d4096e7ec75662a3a4239131e46512ae
Luka Vucer <vucerluka@gmail.com>
2026-05-07 11:41:18 +0200

    feat: LoginView hochgeladen
    Aktuellste Version Hochgeladen.

---
+++ Version 003/loginView.vue
@@ -0,0 +1,46 @@
+<script>
+import LanguageButtons from "@/components/languageButtons.vue";
+
+export default {
+  components: {LanguageButtons},
+  methods: {
+    navigateTobeitragView() {
+      this.$router.push('/topPosts')
+    },
+    navigateToregistrationView() {
+      this.$router.push('/registration')
+    }
+  }
+}
+</script>
+
+<template>
+  <router-view />
+  <div>
+    <h2>Login</h2>
+    <form>
+      <input type="text" placeholder="Benutzername" />
+      <input type="password" placeholder="Passwort" />
+
+      <button @click="navigateTobeitragView">Anmelden</button>
+
+      <button @click="navigateToregistrationView">Registrieren</button>
+
+    </form>
+  </div>
+
+  <languageButtons/>
+
+</template>
+
+<style>
+
+form  {
+  padding: 20px;
+  margin: 60px;
+
+}
+
+
+
+</style>
