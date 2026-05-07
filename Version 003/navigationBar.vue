7e95aec635f0e79fd2932e91c12c03cc92c3fce8
Luka Vucer <vucerluka@gmail.com>
2026-05-07 12:04:24 +0200

    chore: Navbar verschoben/rueberkopiert
    Navbar von Version 002 in Version 003 rueberkopiert.

---
+++ Version 003/navigationBar.vue
@@ -0,0 +1,61 @@
+<script>
+
+export default {
+  methods: {
+    navigateTotopPostsView() {
+      this.$router.push('/topPosts')
+    },
+    navigateTobrowseView() {
+      this.$router.push('/browse')
+    },
+    navigateTocreatePostView() {
+      this.$router.push('/createPost')
+    },
+    navigateToprofileView() {
+      this.$router.push('/profile')
+    }
+  }
+}
+
+
+
+
+
+</script>
+
+<template>
+
+<div>
+  <nav>
+    <ul>
+      <li>
+        <button @click="navigateTotopPostsView">Top Commits</button>
+      </li>
+      <li>
+        <button @click="navigateTobrowseView">Browse</button>
+      </li>
+      <li>
+        <button @click="navigateTocreatePostView">Create Post</button>
+      </li>
+      <li>
+        <button @click="navigateToprofileView">profile</button>
+      </li>
+    </ul>
+
+
+
+
+  </nav>
+</div>
+
+
+
+</template>
+
+<style scoped>
+
+
+
+
+
+</style>
